8df9c07bc3f209daa9e6efdfc19538e60c0e41ad
Itumeleng Kekana <kekanaitumeleng209@gmail.com>
2021-08-11 17:31:10 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const port = process.env.PORT || 5000;
+
+const routes = require('./routes/pageRoutes')
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.listen(port, () =>{
+    console.log('Server up!')
+})
---
+++ routes/pageRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res){
+    res.render('main/home');
+})
+
+module.exports = router;
