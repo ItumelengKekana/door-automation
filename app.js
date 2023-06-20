e9279f84a71cbda571a818548683b93a5030e47c
Itumeleng <itumeleng@letswhoosh.com>
2023-06-20 13:57:10 +0200

    updated port number

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const port = process.env.PORT || 5000;
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const port = process.env.PORT || 5001 || 5000;
 
-const routes = require('./routes/pageRoutes')
+const routes = require("./routes/pageRoutes");
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
-app.listen(port, () =>{
-    console.log(`Server up on ${port}!`)
-})
+app.listen(port, () => {
+	console.log(`Server up on ${port}!`);
+});
